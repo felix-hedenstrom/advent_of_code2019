38d225e5257cfd9fff359b4ba7aface6630147d5
felix <felix.hedenstrom@gmail.com>
2019-12-06 19:46:53 +0100

    Solved d6p2, badly

--- day6/day6.rs
+++ day6/day6.rs
@@ -12,14 +12,13 @@ fn read_stdin() -> String{
 
 #[derive(Debug, Clone)]
 struct Orbit{
-    parent: Option<String>,
     body: String,
     children: HashSet<String>
 }
 
 impl Orbit{
     fn total_suborbits(&self, om: &OrbitMap, own_orbits: usize) -> usize{
-        println!("Checking {:?}", self);
+        //println!("Checking {:?}", self);
         return self.children.iter().map(
             |c|
             own_orbits + match om.omap.get(c){
@@ -48,6 +47,72 @@ impl OrbitMap{
                 }
             ).sum();
     }
+    fn shortest_path(&self, current_position: &String, end: &String) -> Option<usize>{
+        fn shortest_path_internal(om: &OrbitMap, current_position: &String, end: &String, mut visited: HashSet<String>) -> Option<usize>{
+            if current_position == end{
+                return Some(0);
+            }
+
+            let mut shortest: Option<usize> = None;
+
+            for neighbor in om.get_neighbors(current_position){
+                if !visited.contains(&neighbor){
+                    visited.insert(current_position.clone());
+                    let length = shortest_path_internal(om, &neighbor, end, visited.clone());
+
+                    match length{
+                        Some(l) => {
+                            if shortest.is_none() || l + 1< shortest.unwrap() {
+                                shortest = Some(l + 1);
+                            }
+                        },
+                        None => ()
+                    };
+                }
+                
+            }
+            return shortest;
+        }
+    
+        let mut visited = HashSet::new();
+
+        visited.insert(current_position.clone());
+
+        return shortest_path_internal(self, current_position, end, visited).map(|s| s - 2); 
+        
+    }
+    fn get_neighbors(&self, body: &String) -> Vec<String>{
+        let possible_grandparent: Vec<String>  = 
+            self.omap
+            .keys()
+            .filter(
+                |b| 
+                self.omap[b.clone()]
+                .children.contains(body) 
+            ).map(
+                |p| 
+                p.clone()
+            ).collect();
+
+        let grandparent: Option<String> = 
+            match possible_grandparent.len(){
+                0 => None,
+                _ => Some(possible_grandparent[0].clone())
+            };
+
+        let mut neighbors: Vec<String> =  
+            match self.omap.get(body){
+                None => vec![],
+                Some(o) => o.children.iter().map(|c| c.clone()).collect() 
+            };
+        
+        if !grandparent.is_none(){
+            neighbors.push(grandparent.unwrap());
+        }
+        return neighbors;
+        
+        
+    }
     fn new(relations: &Vec<(String, String)>) -> OrbitMap{
         let mut map_contents: HashMap<String, Orbit> = HashMap::new();
          
@@ -68,6 +133,7 @@ impl OrbitMap{
             
             possible_root.remove(child);
 
+
             if map_contents.contains_key(parent){
                 let mut current_orbit = map_contents[parent].clone();
                 current_orbit.children.insert(child.clone()); 
@@ -75,13 +141,12 @@ impl OrbitMap{
             }else{
                 let mut new_children = HashSet::new();
                 
-                let grandparent = relations.iter().filter(|p| (**p).1 == parent).collect::<Vec>()[0];
 
                 new_children.insert(child.clone());
 
                 map_contents.insert(
                     parent.clone(), 
-                    Orbit{body: parent.clone(), children: new_children }
+                    Orbit{body: parent.clone(), children: new_children}
                 );
             }
         }
@@ -111,4 +176,5 @@ fn main (){
     println!("input: {:?}", io_input); 				
     println!("orbit map: {:?}", om);
     println!("total orbits: {:?}", om.total_orbits());
+    println!("Shortest path between YOU and SAN: {:?}", om.shortest_path(&String::from("YOU"), &String::from("SAN")));
 }
